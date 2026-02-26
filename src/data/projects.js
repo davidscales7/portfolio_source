7a1ffcb2b463c608dd9ee0e568438fb89f70e140
davidscales7 <davidscales7@gmail.com>
2026-02-26 20:25:57 +0000

    adding in Descriptions

---
+++ src/data/projects.js
@@ -0,0 +1,14 @@
+export const projects =[
+    {
+        slug:"project-one",
+        name:"Project-One",
+        tagline:"Gives lessons on learning programming + real-time head-to-head coding problem solving.",
+        description:"A learning platform with the goal of teaching programming and letting users compete in real-time on coding challenges",
+        highlights:["Using socket.IO matchmaking + roms for real-time matches",
+            "Embedded Monaco editor with Run + Output for interactive practicing",
+            "Clean UI designed for easy navigation and a polished professional feel",
+        ],
+        stack:["React","Socket.IO", "Monaco Editor", "Node/Express", "Vite"]
+        heroImage:"/projects/projecetone/hero.png",
+    }
+]
